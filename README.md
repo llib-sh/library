9173f3ba8fa378c1c8b6739c160067134b99c531
Mason Wright <mason@lakefox.net>
2021-11-23 22:53:35 -0600

    init

---
+++ README.md
@@ -0,0 +1,110 @@
+<h1 align="center">
+  <br>
+  <a href="http://library.llib.sh"><img src="http://library.llib.sh/logo.png" alt="Library" width="200"></a>
+  <br>
+</h1>
+
+Library is a set of tools that enables super fast streaming by combining torrenting protocols with WebRTC
+
+## How it works
+Library speeds up the downloading process by taking the load one server would normally have, dividing it into 256 packets that are competitively bid on by individuals that will earn based upon how much they contribute to the total file. The process is first started with the requester minting a contract and sending it to a trusted node (dwey). Once the trusted node receives the contract, it publishes it to all the miners that are connected to its WebSocket channel. Then if the miner decides to accept the contract it checks if it has the requested file, collects the requester's webRTC connection information, and forwards its connection information to the requester through the trusted node. When the webRTC connection is finally made the peer sends the packet number it is currently on and the process continues from there where each miner connects and gets sent a packet. After they send the packet the requester sends them the next packet number until they receive all 256 packets it needs. Now that the requester has all the packets it sends on the last request that contains all the payment information. If the receiver fails to send this data it will be charged a fine of 10x the original payment amount to ensure there is no incentive to not pay the miners.
+
+## Install
+
+#### Local Install
+```
+npm install libpkg
+```
+#### CLI Install
+```
+npm install libpkg -g
+```
+#### Library.js
+
+``` html
+<script src="https://unpkg.com/libpkg/library.js"></script>
+```
+
+## Usage
+##### Library.js
+Library.js is the client side framework of the network.
+#### GET file
+  ![](./library_get_example.png)
+####  POST file
+ ![](./library_post_example.png)
+
+## Api
+### `lib = new Library(userId, offer, token)`
+#####  Create a client connection to the network
+_userId_:  Id that the trusted node assigns to your account
+
+_offer_:  Offer to reward the nodes that server the file
+
+_token_: Secret token you share with the trusted node
+
+### `lib.get(fileName)`
+##### Retrive file from network and handle data
+_fileName_: SHA256 hash of file (returned by "lib.post")
+
+##### returns:
+**[Promise]** [_Responce Object_](https://developer.mozilla.org/en-US/docs/Web/API/Response): Output is calculated as below
+```javascript
+// file = "7dsa968d6a7s86d7ysa7d9..." (file data returned by nodes)
+
+// Convert "file" to base64
+
+return new Promise(async function(resolve, reject) {
+  let b64 = btoa(file);
+  // Attempt to find file format if possible
+  let mime = detectMimeType(base64);
+  // fetch base64 URL of the file data and return the promise
+  resolve(fetch(`data:${mime};base64,${base64}`).catch(reject))
+})
+
+```
+###### Responce Object Methods
+_res.text()_: returns a promise the resolves with a plain text responce
+
+_res.json()_: returns a promise the resolves with a parsed JSON responce
+
+_res.blob()_: returns a promise the resolves with a blob of the provided data
+
+_res.arrayBuffer()_: returns a promise the resolves with a ArrayBuffer of the provided data
+
+_res.clone()_: returns a promise the resolves with a clone of the provided data
+
+
+### `lib.post(fileData)`
+##### Publish file to the network
+_fileData_: Any string that contains data you want to store on the network
+
+**NOTE**: fileData _must_ be a String, any other type _will_ be rejected
+
+##### returns:
+**[Promise]** _fileName_ : A SHA256 hash of the file, used as the fileId to retrive the file
+
+## Librarian
+Librarian is the CLI node, below describes the usage of it.
+##### Usage
+```
+librarian --f <files> -t <token> -a <user> -o <offer>
+```
+###### Display settings
+_-q_, --quiet: **[Boolean]** Quiet mode
+
+_-f_, --files: **[String]** Route to store hosted files
+
+_-t_, --token: **[String]** Your unique token given at sign up
+
+_-u_, --user: **[String]** Your user id given at sign up
+
+_-o_, --offer: **[Boolean]** Your minimum accepted offer in cents USD
+
+_-l_, --log: **[Boolean]** Optional, Enable logging
+
+_-q_, --quiet: **[Boolean]** Optional, Quiet mode
+
+_-h_, --host: **[String]** Optional, URL of your preferred trusted node. _Defaults to "http://llib.sh/dwey"_
+
+##### Default layout
+  ![](./librarian.png)
---
+++ librarian.js
@@ -0,0 +1,197 @@
+#! /usr/bin/env node
+const fs = require('fs');
+const ch = require('cipherhash');
+const io = require('socket.io-client');
+const path = require('path');
+const Peer = require('simple-peer');
+const wrtc = require('wrtc');
+const yargs = require('yargs');
+const pepsin = require('pepsin');
+const {table,getBorderCharacters} = require('table');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+const options = yargs
+ .usage("Usage: -f <files> -t <token> -a <user> -o <offer>")
+ .option("f", { alias: "files", describe: "Route to store hosted files", type: "string", demandOption: true })
+ .option("t", { alias: "token", describe: "Your unique token given at sign up", type: "string", demandOption: true })
+ .option("u", { alias: "user", describe: "Your user id given at sign up", type: "string", demandOption: true })
+ .option("o", { alias: "offer", describe: "Your minimum accepted offer in cents USD", type: "string", demandOption: true })
+ .option("l", { alias: "log", describe: "Enable logging", type: "boolean" })
+ .option("q", { alias: "quiet", describe: "Quiet mode", type: "boolean" })
+ .option("h", { alias: "host", describe: "Third party host api", type: "string" })
+ .argv;
+
+let host = options.host || "http://llib.sh/dwey";
+const socket = io(`ws://${new URL(host).hostname}${new URL(host).port!=""?":"+new URL(host).port:""}/`);
+let filePath = options.files; // ./files
+let token = options.token; // "sdajklhjlkhdsajkhsadhjkhjlkdsahjlksadjlhkjldhksa"
+let user = options.user; // "618ea428eac7f3bf126031b8"
+let minOffer = options.offer; // 0.01
+let logging = options.log;
+let quiet = options.quiet;
+
+if (fs.existsSync(path.join(filePath,filePath+".json"))) {
+  fileMap = JSON.parse(fs.readFileSync(path.join(filePath,filePath+".json")));
+} else {
+  fileMap = [];
+  fs.writeFileSync(path.join(filePath,filePath+".json"), "[]");
+}
+
+const stats = [
+  ['Stat',"Value"],
+  ['Total Files', fileMap.length],
+  ['Total Requests', 0],
+  ['Total Packets [15kb]', 0],
+  ['Current Requests', 0]
+];
+
+const config = {
+  header: {
+    alignment: 'center',
+    content: 'LIBRARIAN\nLive Stat Read-Out\nRoot: '+filePath,
+  },
+  border: getBorderCharacters(`norc`)
+}
+
+let logs = ``;
+let log = () => {}
+
+if (logging && !quiet) {
+  log = console.log;
+} else if (!quiet) {
+  log = (msg) => {
+    logs += msg+"\n";
+  };
+  setInterval(() => {
+    console.clear();
+    stats[1][1] = fileMap.length;
+    console.log(center(table(stats, config)));
+  },50)
+}
+
+socket.on("connect", (msg) => {
+  log("connect");
+});
+
+socket.on("REQ", async (msg) => {
+  log(msg);
+  stats[2][1] += 1;
+  stats[4][1] += 1;
+  let msgSplit = msg.split(" ");
+  let contractId = msgSplit[0];
+  let fileId = msgSplit[1];
+  let offer = parseFloat(msgSplit[2]);
+  if (fileMap.indexOf(fileId) == -1) {
+    const res = await fetch(`${host}/download/${fileId}`);
+    await new Promise((resolve, reject) => {
+      const fileStream = fs.createWriteStream(path.join(filePath,fileId+".lib"));
+      res.body.pipe(fileStream);
+      res.body.on("error", (err) => {
+        reject(err);
+      });
+      fileStream.on("finish", function() {
+        fileMap.push(fileId);
+        fs.writeFileSync(path.join(filePath,filePath+".json"), JSON.stringify(fileMap));
+        resolve();
+      });
+    });
+  }
+  if (offer >= minOffer) {
+    const peer = new Peer({wrtc: wrtc});
+    let myId = pepsin();
+    log(myId);
+    let sdpSize = 0;
+    socket.on("R"+myId, (data) => {
+      log("RX");
+      log(data);
+      sdpSize = parseInt(data.sdp.trim().split("\n").pop().split(":")[1])-300;
+      stats[3][0] = `Total Packets [${parseInt(sdpSize/1000)}kb]`
+      log(sdpSize);
+      let sent = false;
+      peer.on("signal", (mySig) => {
+        if (sent) {
+          return ;
+        }
+        sent = true
+        log(mySig);
+        log("emit", contractId);
+        socket.emit("contract", {data:{data:mySig,id: myId,action:"CONN INFO"},id:contractId});
+      });
+      peer.signal(data);
+      log(path.join(filePath,"./"+fileId+".lib"));
+      fs.readFile(path.join(filePath,"./"+fileId+".lib"), (err, fileBuffer) => {
+        if (!err) {
+          let fbSplit = fileBuffer.toString("binary").split("");
+          let file = [];
+          if (fbSplit.length/256 >= sdpSize) {
+            file = chunk(fbSplit,fbSplit.length/256);
+          } else {
+            file = chunk(fbSplit,sdpSize);
+          }
+          peer.on('data', async (packetid) => {
+            log(packetid.toString());
+            let packetSplit = packetid.toString().split(" ");
+            if (packetSplit[0] == "PACKET") {
+              if (parseInt(packetSplit[1]) <= file.length-1) {
+                let packet = file[parseInt(packetSplit[1])];
+                let chunks = [packet]
+                if (packet.length > sdpSize) {
+                  chunks = chunk(packet, sdpSize);
+                }
+                chunks.forEach((item, i) => {
+                  stats[3][1] += 1;
+                  log(`Sending Packet: ${packetSplit[1]} Chunk: ${chunks.length-1} Size: ${item.length}`);
+                  peer.send(`DATA ${packetSplit[1]} ${i} ${chunks.length-1} ${auth(token)} ${user} ${item.join("")}`);
+                });
+              } else {
+                stats[4][1] -= 1;
+                log("Killing");
+                peer.send(`DATA END ${packetSplit[1]}`);
+              }
+            }
+          });
+        }
+      });
+
+      peer.on('disconnect', () => {
+        log("Connection closed");
+        peer.destroy();
+      });
+    });
+    // Init the connection
+    log("Emitting");
+    socket.emit("contract", {
+      data: {
+        id: myId,
+        action: "REQ CONN"
+      },
+      id: contractId
+    });
+  }
+});
+
+socket.on("disconnect", (msg) => {
+  log("disconnect");
+});
+
+function auth(token) {
+  let time = parseInt(new Date().getTime()/30000);
+  return ch.cipherHash(time.toString(),token);
+}
+
+function chunk(arr,n){
+  var r = Array(Math.ceil(arr.length/n)).fill();
+  return r.map((e,i) => arr.slice(i*n, i*n+n));
+}
+
+function center(text) {
+  let t = text.split("\n");
+  let width = parseInt((process.stdout.columns/2)-(t[0].length/2));
+  let height = parseInt((process.stdout.rows/2)-(t.length/2));
+  // console.log(height);
+  for (var i = 0; i < t.length; i++) {
+    let line = t[i];
+    t[i] = new Array(Math.max(width,0)).fill(" ").join("")+t[i];
+  }
+  return new Array(Math.max(height,0)).fill("\n").concat(t.join("\n")).concat(new Array(Math.max(height,0)).fill("\n").join("")).join("");
+}
